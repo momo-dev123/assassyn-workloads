cc458f16a483cc0e5e66c5eacab8655ef034ab8b
were <jian.weng@kaust.edu.sa>
2025-03-28 18:09:42 +0000

    remove it

--- sv-source/gemini-mesh/synthesize.sh
+++ sv-source/gemini-mesh/synthesize.sh
@@ -5,10 +5,10 @@ mkdir -p "$XDG_RUNTIME_DIR"
 
 apptainer exec \
     --bind ./MacUnit.sv:/home/ubuntu/MacUnit.sv \
-    --bind ./PE.sv:/home/ubuntu/PE.sv \
+    --bind ./PE_16.sv:/home/ubuntu/PE_16.sv \
     $WORKLOAD_HOME/open_eda.sif \
-    sc /home/ubuntu/MacUnit.sv /home/ubuntu/PE.sv \
-    -design PE \
+    sc /home/ubuntu/MacUnit.sv /home/ubuntu/PE_16.sv \
+    -design PE_16 \
     -target asap7_demo -define SYNTHESIS=1
 
 cp ./build/PE/job0/syn/0/reports/stat.json ../../plot/reports/gemmini-pe.json
